323e82063a4c2b36d57f2ca1db920832bf1a5642
alafty <mohamedelelafty@yahoo.com>
2022-07-25 15:57:49 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mohamed El-Alafty on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
